848414760323f20ba6cc65636d9bd5589c8bf0f8
katsun0921 <s.katsumasa@gmail.com>
2019-11-24 00:16:07 +0900

    スキルリンクを変更

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
     <!-- クリック時に.is-reverseを付与-->
     <div class="js-topName p-top__nameContainer">
       <h1 class="p-top__headingMain">Katsumasa<br>Sato<span class="p-top__headingMain--sub">WEB DEVELOPER</span></h1>
-      <a class="p-top__nameSkillLink" href="https://drive.google.com/open?id=1pWnjGrUb1viZDfJ0AbazUe1gE14RgJuNk8UmCin2Xk4" target="_blank" rel="noopener">SKILL SHEET</a>
+      <a class="p-top__nameSkillLink" href="https://docs.google.com/spreadsheets/d/1pWnjGrUb1viZDfJ0AbazUe1gE14RgJuNk8UmCin2Xk4/edit?usp=sharing" target="_blank" rel="noopener">SKILL SHEET</a>
     </div>
   </section>
   <nav class="l-menu__blocks" id="js-topMenu">
